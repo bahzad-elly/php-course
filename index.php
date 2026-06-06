06e3e75184ae70e85608fa3a13127bb0655747c0
bahzadcomputer <bahzadcomputer@gmail.com>
2026-06-06 19:36:00 +0300

    day 4 is done

--- index.php
+++ index.php
@@ -1,8 +1,61 @@
 <?php
-echo "hello world";
+
+
+// type of comments in programming 
+
+// single line
+# single line 
+
+/*
+
+mulit line comment 
+*/
+
+/***
+ * name function is sum
+ * parat
+ */
+
+// integer
+$num = 19;
+//string
+$name = "php";
+
+//boolean 
+$result = true;
+$res = false;
+
+//double / fload 
+
+$numone = 23.5;
+
+//array
+
+
+$arr = ["red","green","blue"];
+
+
+// assoccaitive array
+
+$user = [
+    "name" => "bahzad",
+    "age"=> 22,
+    "gender"=>"male"
+
+];
+
+// object 
+
+class User{
+    public $name = "bahzad";
+}
+
+$objClass = new User();
+// null
+
+$isTrue= null;
+
+var_dump($isTrue);
 
 ?>
 
-<?php
-echo "</br>"."hello ,php";
-?>
