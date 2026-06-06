589c109e535b46eb8db14e25b0f145ae93466d12
bahzadcomputer <bahzadcomputer@gmail.com>
2026-06-07 00:18:41 +0300

    day 5 , lesson (operators )

---
+++ operator.php
@@ -0,0 +1,31 @@
+<?php
+/*
+
+type of operators :
+
+arithmetic operator 
+assignment operator
+
+comparsion operator
+
+increment / decrement operator
+
+logical operator
+
+string operator
+
+array operator 
+
+conditional (ternary) operator
+
+null coalscing operator
+
+ */
+
+$age = null;
+
+echo $age?? "guset";
+
+
+
+?>
