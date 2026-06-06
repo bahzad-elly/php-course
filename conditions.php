a9d6f123e833092c42b9a98556aed3298913a873
bahzadcomputer <bahzadcomputer@gmail.com>
2026-06-07 00:29:11 +0300

    day 6 , conditions part 1

---
+++ conditions.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+type of contions :
+if
+else
+elseif
+
+nested if
+
+
+switch
+match
+
+
+*/
+
+$age = 34;
+if($age<= 30 || $age >= 12 ){
+    echo "the age is perfect";
+    if($age>32){
+        echo "the age is bigger than 32";
+    }
+}else{
+    echo "the age is small";
+}
+
+
+
+?>
