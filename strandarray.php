676e6c7cd0f5d56b1b0362590ef087fce4ff2f35
bahzadcomputer <bahzadcomputer@gmail.com>
2026-06-10 00:19:04 +0300

    array and string functions are doen

---
+++ strandarray.php
@@ -0,0 +1,44 @@
+<?php
+//this is some function for string and array.
+
+$text = "hello world";
+
+// echo strlen($text);
+
+// echo str_word_count($text);
+
+// echo strtoupper($text);
+
+// echo strtolower($text);
+
+// echo ucfirst($text);
+
+// echo str_replace("world","php",$text);
+
+// echo strpos($text,"world");
+
+// echo substr($text,4,8);
+
+
+// $arr = ["red","green","blue","black","white"];
+// $num = [1,3,4,34,54,0,6];
+// rsort($num);
+// print_r($num);
+
+// echo count($arr);
+
+// array_push($arr,"orange"); 
+
+// print_r($arr);
+
+// if(in_array("greenn",$arr)){
+//     echo "found";
+// }else{
+//     echo "not found";
+// }
+
+
+
+
+
+?>
