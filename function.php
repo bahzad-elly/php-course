6a52603a890593c59c6f8eed1dc07a7e46d38d21
bahzadcomputer <bahzadcomputer@gmail.com>
2026-06-10 14:06:20 +0300

    fucntions are done

---
+++ function.php
@@ -0,0 +1,65 @@
+<?php
+
+// functions : 
+
+
+// first function .
+// function sayHello(){
+//     echo "hello world</br>";
+// }
+// sayHello();
+// sayHello();
+
+// built-in function : strlent() ...
+
+
+//user defined fucntion 
+
+/*
+function name_function(){
+
+}
+
+
+call_function();
+*/
+// parameter .
+// function welcome($name){
+//     echo "welcome : $name";
+// }
+
+// welcome("ali");
+
+
+// return
+// function sum($num1,$num2){
+//     return $num1 + $num2;
+// }
+
+// echo sum(4,5);
+
+//anonymouse function 
+// $hello = function(){
+//     echo "hello world";
+// };
+
+// $hello();
+
+
+//arrow function
+
+// $sum = fn($a,$b)=> $a+$b;
+
+// echo $sum(6,7);
+
+
+function buttons($text){
+    echo "<button>$text</button>";
+}
+
+buttons("save");
+buttons("delete");
+buttons("add");
+buttons("edit");
+
+?>
