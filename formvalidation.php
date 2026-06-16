fee82bfb06565ecb8b0d099930c7fc72c4232d86
bahzadcomputer <bahzadcomputer@gmail.com>
2026-06-16 14:25:55 +0300

    form validation and error message is done

--- formvalidation.php
+++ formvalidation.php
@@ -4,17 +4,47 @@ if($_POST){
     $pass = $_POST['pass'];
     $confirm = $_POST['confirm'];
     $age = $_POST['age'];
+
+    $error = "";
+
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $error = "you need add the true email";
+    }
     if(empty($email)){
-        echo "  <script>alert('you need add the email');</script>";
+        $error = "you need put the email";
     }
 
     if(strlen($pass) < 8 ){
-
-     echo "  <script>alert('you need add password srong');</script>";
+        $error = "the password is not strong";
     }if(!is_numeric($age)){
-        echo " <script>alert('you need add the age');</script>";
+        $error = "you need add the number";
     }
     }
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <h3 style="color:red;">
+            <?php
+                echo $error;
+            ?>
+        </h3>
+        email <input type="text" name="email" ><br>
+        password <input type="password" name="pass"><br>
+        comfirm password <input type="password" name="confirm"><br>
+        <br> age <input type="text" name="age"><br>
+        <button type="submit">send</button>
+        
+    </form>
+  
+    
+</body>
+</html>
